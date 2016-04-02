cf90e65d8054391a3b4e67162ab53a5e42d3e1a0
Alexander Immer <alexander.immer@yahoo.com>
2016-04-02 18:56:48 +0200

    Adapt handlers to refactoring

--- Backend/app/handlers.py
+++ Backend/app/handlers.py
@@ -1,8 +1,8 @@
 from flask_restful import Resource
 from flask import request
-from app.credentials import access_token
+
 from app.model import ActiveUsers
-import requests
+from app.recipes import RecipeProvider
 
 active_users = ActiveUsers()
 
@@ -18,39 +18,11 @@ class Session(Resource):
 
 
 class Match(Resource):
-    base_url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com'
-    headers = {
-        'X-Mashape-Key': access_token,
-        'Accept': 'application/json'
-    }
-
-    @classmethod
-    def best_recipe(cls, ingredients):
-        params = {
-            'ingredients': ingredients,
-            'limitLicense': False,
-            'ranking': 2
-        }
-        recipes = requests.get(cls.base_url + '/recipes/findByIngredients',
-                               headers=cls.headers, params=params).json()
-        if not recipes:
-            return None
-        recipe = recipes.sort(key=lambda r: r.get('likes'), reverse=True)[0]
-        return recipe
-
-    @classmethod
-    def recipe(cls, _id):
-        params = {
-            'includeNutrition': False
-        }
-        return requests.get(cls.base_url + '/recipes/{}/information'.format(_id),
-                            headers=cls.headers, params=params).json()
-
     @classmethod
     def get(cls):
         ingredients = ','.join(active_users.joined_ingredients())
-        recipe = cls.best_recipe(ingredients)
+        recipe = RecipeProvider.best_recipe(ingredients)
         if recipe:
-            return cls.recipe(recipe['id'])
+            return RecipeProvider.recipe(recipe['id'])
         else:
             return {}
