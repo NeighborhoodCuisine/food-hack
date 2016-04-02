6a833e0c662f0b5654d50c1e5d7808fd74e172eb
Sven Mkw <sven.mkw@gmail.com>
2016-04-02 14:39:32 +0200

    included main view with navigation

--- NeighborhoodCuisine/routes/main.js
+++ NeighborhoodCuisine/routes/main.js
@@ -1,15 +1,30 @@
 import React, { Component,
-         View,
-         Text } from 'react-native'
+                View,
+                Text,
+                StyleSheet } from 'react-native'
 
 class Main extends Component {
+  routeToFilter() {
+    console.log('route to filter')
+  }
+
   render() {
+    console.log('render me')
     return (
       <View>
-        <Text>Main!</Text>
+        <Text style={styles.headline}>Hungry?</Text>
+        <Text
+          onPress={this.routeToFilter}
+          style={styles.button}>Find your Food Mates.
+        </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  headline: {},
+  button: {}
+})
 
 module.exports = Main
