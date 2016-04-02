d63d56af786f0ca3aa1e8c66051f745b4cbd7047
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2016-04-02 14:36:18 +0200

    Add credentials

--- Backend/app/handlers.py
+++ Backend/app/handlers.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource
 from flask import request
+import requests
+from app.credentials import *
 
 
 class Session(Resource):
-
     @staticmethod
     def post():
         data = request.get_json()
         print(data)
+
