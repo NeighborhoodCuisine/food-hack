6b10a3953829dfb1dbf018fd59181ddfb4b05f57
Willi Raschkowski <willi.raschkowski@student.hpi.de>
2016-04-02 16:46:55 +0200

    Add Match route

--- Backend/app/handlers.py
+++ Backend/app/handlers.py
@@ -1,7 +1,8 @@
 from flask_restful import Resource
-from flask import request
-
+from flask import request, jsonify
+from app.credentials import access_token
 from app.model import ActiveUsers
+import requests
 
 active_users = ActiveUsers()
 
@@ -20,3 +21,20 @@ class Ingredients(Resource):
     def get():
         return {}
 
+
+class Match(Resource):
+    @staticmethod
+    def get():
+        ingredients = ','.join(active_users.joined_ingredients())
+        header = {
+            'X-Mashape-Key': access_token,
+            'Accept': 'application/json'
+        }
+        params = {
+            'ingredients': ingredients,
+            'limitLicense': False,
+            'ranking': 2
+        }
+        response = requests.get('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients',
+                                header=header, params=params).json()
+        return jsonify(response)
