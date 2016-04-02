b9f69c58c813d9e64b53a4d07032126fcd9ab7a3
Alexander Immer <alexander.immer@yahoo.com>
2016-04-02 12:54:37 +0200

    Change session handler

--- Backend/app/handlers.py
+++ Backend/app/handlers.py
@@ -1,11 +1,17 @@
-from flask import request
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument('test')
+parser.add_argument('ses')
 
 
 class Session(Resource):
 
     @staticmethod
+    def get():
+        return {'get': 'test'}
+
+    @staticmethod
     def post():
-        data = request.get_json()
-        # debug printing
+        data = parser.parse_args()
         print(data)
